3b3c4a092ad1b19baf1aad3d69cbcbf781ee81ee
Kristófer R <kristofer.thorlaksson@automattic.com>
2021-07-14 12:38:25 +0000

    Use Tailwind for CSS styling

---
+++ assets/tailwind.config.js
@@ -0,0 +1,19 @@
+module.exports = {
+  purge: [
+    '../lib/**/*.ex',
+    '../lib/**/*.leex',
+    '../lib/**/*.eex',
+    './js/**/*.js'
+  ],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    container: {
+      center: true
+    },
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
