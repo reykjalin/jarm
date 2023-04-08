5c317b557f3b8f35cc0b835614a6a1ad40ec7b51
Kristófer R <kristofer.thorlaksson@automattic.com>
2023-04-08 03:07:23 -0400

    remove io inspects

--- lib/inner_circle_web/locale.ex
+++ lib/inner_circle_web/locale.ex
@@ -8,14 +8,11 @@ defmodule InnerCircleWeb.Locale do
   def set_locale_cookie(conn, opts) do
     locale = Gettext.get_locale(opts[:gettext])
 
-    IO.inspect(locale, label: "current locale")
-
     persist_locale(conn, locale)
   end
 
   defp persist_locale(%Conn{cookies: %{@cookie => saved_locale}} = conn, new_locale) do
     if new_locale != saved_locale do
-      IO.inspect(new_locale, label: "persisting locale")
       Conn.put_resp_cookie(conn, @cookie, new_locale, max_age: @ten_days)
     else
       conn
@@ -25,7 +22,6 @@ defmodule InnerCircleWeb.Locale do
   defp persist_locale(conn, nil), do: conn
 
   defp persist_locale(conn, locale) do
-    IO.inspect(locale, label: "persisting locale")
     Conn.put_resp_cookie(conn, @cookie, locale, max_age: @ten_days)
   end
 end
