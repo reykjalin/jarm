d66fcb74335f5573101dcbce1736150cda970b30
Kristófer R <kristofer.thorlaksson@automattic.com>
2023-07-14 02:01:18 -0400

    add emoji search

--- lib/jarm_web/live/post_live/timeline_post_component.ex
+++ lib/jarm_web/live/post_live/timeline_post_component.ex
@@ -3,5 +3,22 @@ defmodule JarmWeb.PostLive.TimelinePostComponent do
 
   alias Phoenix.LiveView.JS
 
+  alias Jarm.Emojis
+
   import Canada, only: [can?: 2]
+
+  @impl true
+  def handle_event("search_emoji", %{"query" => query}, socket) do
+    emojis = case query do
+      "" -> Emojis.all_emojis()
+      query -> Emojis.search_emojis(query)
+    end
+
+    socket =
+      socket
+      |> assign(emojis: emojis)
+
+    {:noreply, socket}
+  end
+
 end
