d84ddc30bb26e4481e7e3795a9490bfe93ef22a9
Kristófer R <kristofer.thorlaksson@automattic.com>
2023-07-21 05:02:18 -0400

    remove log from restore locale pipeline

--- lib/jarm_web/live/restore_locale.ex
+++ lib/jarm_web/live/restore_locale.ex
@@ -1,6 +1,5 @@
 defmodule JarmWeb.RestoreLocale do
   def on_mount(:default, %{"locale" => locale}, _session, socket) do
-    IO.inspect(socket, label: "restore locale socket")
     Gettext.put_locale(JarmWeb.Gettext, locale)
     socket = Phoenix.Component.assign(socket, locale: locale)
     {:cont, socket}
