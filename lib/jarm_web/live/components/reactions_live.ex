f40382a5eb37b0eec037f3e48e2e14598a73f626
Kristófer R <kristofer.thorlaksson@automattic.com>
2023-09-03 22:23:07 -0400

    fix wrapping issue on reaction button

--- lib/jarm_web/live/components/reactions_live.ex
+++ lib/jarm_web/live/components/reactions_live.ex
@@ -59,7 +59,7 @@ defmodule JarmWeb.LiveComponents.ReactionsLive do
 
       <div class="flex flex-row gap-5 items-center">
         <button
-          class="my-1 py-2 px-2 md:py-2 md:px-3 light:hover:bg-slate-100 hover:bg-slate-600 rounded border hover:border-slate-400"
+          class="my-1 py-2 px-2 md:py-2 md:px-3 light:hover:bg-slate-100 hover:bg-slate-600 rounded border hover:border-slate-400 whitespace-nowrap"
           phx-click={
             JS.toggle(
               to: "#post-#{@post_id}-reactions",
