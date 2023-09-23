9cda1789a14c832756c0fb5811168071d7143984
Kristófer R <kristofer.thorlaksson@automattic.com>
2023-09-23 16:57:14 -0400

    Revert "add extra space to generated avatar images so we can apply css effects"
    This reverts commit 0b3369b17585052a1a6c5405affb9963147566ab.

--- lib/avatar-generators/cat/avatar.ex
+++ lib/avatar-generators/cat/avatar.ex
@@ -19,6 +19,6 @@ defmodule AvatarGenerators.Cat do
       "accessorie_#{:rand.uniform(20)}.png"
     ]
 
-    ImageMagick.generate_avatar_from_parts(parts, output, size)
+    ImageMagick.layer_images_on_top_of_each_other(parts, output, size)
   end
 end
