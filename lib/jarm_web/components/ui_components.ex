ea9f8cdffaf6e61ef12162d5670dbca7a68db958
Kristófer R <kristofer.thorlaksson@automattic.com>
2023-10-14 02:26:46 -0400

    update media list ui

--- lib/jarm_web/components/ui_components.ex
+++ lib/jarm_web/components/ui_components.ex
@@ -14,4 +14,18 @@ defmodule JarmWeb.UiComponents do
     </div>
     """
   end
+
+  attr :class, :string, default: nil
+  slot :inner_block, required: true
+
+  def code(assigns) do
+    ~H"""
+    <code class={[
+      "m-1 p-2 border border-solid rounded-md text-orange-500 bg-slate-600 light:bg-gray-200",
+      @class
+    ]}>
+      <%= render_slot(@inner_block) %>
+    </code>
+    """
+  end
 end
--- lib/jarm_web/live/admin_live/media_list.ex
+++ lib/jarm_web/live/admin_live/media_list.ex
@@ -9,7 +9,7 @@ defmodule JarmWeb.AdminLive.MediaList do
 
     socket =
       assign(socket,
-        media: if(media, do: media, else: []),
+        media: if(media, do: Enum.reverse(media), else: []),
         locale: locale
       )
 
@@ -22,71 +22,86 @@ defmodule JarmWeb.AdminLive.MediaList do
     <h1 class="text-3xl pb-5"><%= gettext("Media") %></h1>
 
     <.card>
-      <.table id="media" rows={@media}>
-        <:col :let={m} label={gettext("ID")}><%= m.id %></:col>
-        <:col :let={m} label={gettext("Post ID")}>
-          <.link href={~p"/#{@locale}/posts/#{m.post_id}"}><%= m.post_id %></.link>
-        </:col>
-        <:col :let={m} label={gettext("MIME type")}><%= m.mime_type %></:col>
-        <:col :let={m} label={gettext("Size")}><%= m.width %>x<%= m.height %></:col>
-        <:col :let={m} label={gettext("Path to original")}>
-          <.icon :if={not File.exists?(m.path_to_original)} name="hero-x-mark" class="text-red-500" />
-          <.icon :if={File.exists?(m.path_to_original)} name="hero-check" class="text-green-500" />
-          <code>
-            <%= m.path_to_original %>
-          </code>
-        </:col>
-        <:col :let={m} label={gettext("Path to compressed")}>
-          <.icon
-            :if={
+      <details :for={m <- @media} class="py-5 border-b border-solid last:border-none">
+        <summary><%= m.id %></summary>
+        <p class="my-2">ID: <%= m.id %></p>
+
+        <p class="my-2">
+          Post ID: <.link href={~p"/#{@locale}/posts/#{m.post_id}"}><%= m.post_id %></.link>
+        </p>
+
+        <div class="my-2">
+          <p class="my-1">
+            Path to original:
+            <span :if={not File.exists?(m.path_to_original)}>
+              <.icon name="hero-x-mark" class="text-red-500" />
+            </span>
+            <span :if={File.exists?(m.path_to_original)}>
+              <.icon name="hero-check" class="text-green-500" />
+              <.link href={~p"/media/#{m.uuid}"}>[direct link]</.link>
+            </span>
+          </p>
+          <pre class="overflow-auto border border-solid p-2"><%= m.path_to_original %></pre>
+        </div>
+
+        <div class="my-2">
+          <p class="my-1">
+            Path to compressed:
+            <span :if={
               m.path_to_compressed == nil or m.path_to_compressed == "" or
                 not File.exists?(m.path_to_compressed)
-            }
-            name="hero-x-mark"
-            class="text-red-500"
-          />
-          <.icon
-            :if={
+            }>
+              <.icon name="hero-x-mark" class="text-red-500" />
+            </span>
+            <span :if={
               m.path_to_compressed != nil and m.path_to_compressed != "" and
                 File.exists?(m.path_to_compressed)
-            }
-            name="hero-check"
-            class="text-green-500"
-          />
-          <code>
-            <%= if m.path_to_compressed == nil or m.path_to_compressed == "",
-              do: "-",
-              else: m.path_to_compressed %>
-          </code>
-        </:col>
-        <:col :let={m} label={gettext("Path to thumbnail")}>
-          <.icon
-            :if={
+            }>
+              <.icon name="hero-check" class="text-green-500" />
+              <.link href={~p"/compressed-media/#{m.uuid}"}>[direct link]</.link>
+            </span>
+
+            <.button class="bg-green-500">
+              <.icon name="hero-arrow-path" />
+            </.button>
+          </p>
+          <pre class="overflow-auto border border-solid p-2"><%= if m.path_to_compressed == nil or m.path_to_compressed == "", do: "-", else: m.path_to_compressed %></pre>
+        </div>
+
+        <div :if={String.starts_with?(m.mime_type, "image")} class="my-2">
+          <p class="my-1">
+            <%= gettext("Path to thumbnail") %>:
+            <span :if={
               m.path_to_thumbnail == nil or m.path_to_thumbnail == "" or
                 not File.exists?(m.path_to_thumbnail)
-            }
-            name="hero-x-mark"
-            class="text-red-500"
-          />
-          <.icon
-            :if={
+            }>
+              <.icon name="hero-x-mark" class="text-red-500" />
+            </span>
+            <span :if={
               m.path_to_thumbnail != nil and m.path_to_thumbnail != "" and
                 File.exists?(m.path_to_thumbnail)
-            }
-            name="hero-check"
-            class="text-green-500"
-          />
-          <code>
-            <%= if m.path_to_thumbnail == nil or m.path_to_thumbnail == "",
-              do: "-",
-              else: m.path_to_thumbnail %>
-          </code>
-        </:col>
-        <:col :let={m} label={gettext("LQIP")}>
-          <.icon :if={m.lqip == nil or m.lqip == ""} name="hero-x-mark" class="text-red-500" />
-          <.icon :if={m.lqip != nil and m.lqip != ""} name="hero-check" class="text-green-500" />
-        </:col>
-      </.table>
+            }>
+              <.icon name="hero-check" class="text-green-500" />
+              <.link href={~p"/thumbnail/#{m.uuid}"}>[direct link]</.link>
+            </span>
+            <.button class="bg-green-500">
+              <.icon name="hero-arrow-path" />
+            </.button>
+          </p>
+          <pre class="overflow-auto border border-solid p-2"><%= if m.path_to_thumbnail == nil or m.path_to_thumbnail == "", do: "-", else: m.path_to_thumbnail %></pre>
+        </div>
+
+        <div class="my-2">
+          <p>
+            <%= gettext("LQIP") %>:
+            <.icon :if={m.lqip == nil or m.lqip == ""} name="hero-x-mark" class="text-red-500" />
+            <.icon :if={m.lqip != nil and m.lqip != ""} name="hero-check" class="text-green-500" />
+            <.button class="bg-green-500">
+              <.icon name="hero-arrow-path" />
+            </.button>
+          </p>
+        </div>
+      </details>
     </.card>
     """
   end
